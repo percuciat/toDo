1e81c93e22d46f661f4c11536526aa5ba1d046fc
percuciat <zerob.rob@yandex.ru>
2020-07-26 13:02:53 +0300

    Updates

---
+++ precache-manifest.03e69fdbd28fceb9d2970defb3e78bb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babea216eee303bb8e718fb1e9b3e15c",
+    "url": "/toDo/index.html"
+  },
+  {
+    "revision": "0829b3a07e4d2629a2cc",
+    "url": "/toDo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "12f36b70fe783fd11261",
+    "url": "/toDo/static/css/main.6bf1ce38.chunk.css"
+  },
+  {
+    "revision": "0829b3a07e4d2629a2cc",
+    "url": "/toDo/static/js/2.bcc8448f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/toDo/static/js/2.bcc8448f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f36b70fe783fd11261",
+    "url": "/toDo/static/js/main.63276d7f.chunk.js"
+  },
+  {
+    "revision": "0e2d6f08773cafbb6df3",
+    "url": "/toDo/static/js/runtime-main.653b1aad.js"
+  }
+]);
